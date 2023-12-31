8bb461b50529b69cbe0c2c7c42945d695e20f7b3
ufukyazici <ufukyazici.dev@gmail.com>
2023-12-31 17:14:44 +0300

    Refactor incrementPageIndex method in OnBoardCubit

--- lib/feature/onboard/view_model/on_board_cubit.dart
+++ lib/feature/onboard/view_model/on_board_cubit.dart
@@ -6,14 +6,12 @@ part 'on_board_state.dart';
 class OnBoardCubit extends Cubit<OnBoardState> {
   OnBoardCubit() : super(const OnBoardState());
   void incrementPageIndex() {
-    _isLastIndex(state.pageIndex + 1);
-  }
-
-  void _isLastIndex(int index) {
-    if (index < 2) {
-      emit(state.copyWith(pageIndex: index));
-    } else if (index == 2) {
-      emit(state.copyWith(pageIndex: index, isLastIndex: true));
+    if (!_isLastIndex) {
+      emit(state.copyWith(pageIndex: state.pageIndex + 1));
+    } else {
+      emit(state.copyWith(pageIndex: state.pageIndex + 1, isLastIndex: true));
     }
   }
+
+  bool get _isLastIndex => state.pageIndex + 1 == 2;
 }
