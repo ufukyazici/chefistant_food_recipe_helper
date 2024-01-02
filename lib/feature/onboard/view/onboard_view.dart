fc6312569fd0e38c6053859353c8cc38b2a6dd4c
ufukyazici <ufukyazici.dev@gmail.com>
2024-01-02 17:43:50 +0300

    Add page indicators to OnBoardView

--- lib/feature/onboard/view/onboard_view.dart
+++ lib/feature/onboard/view/onboard_view.dart
@@ -38,6 +38,26 @@ class _OnBoardViewState extends State<OnBoardView> {
               Expanded(
                 child: _buildPageView(),
               ),
+              BlocSelector<OnBoardCubit, OnBoardState, int>(
+                selector: (state) {
+                  return state.pageIndex;
+                },
+                builder: (context, state) {
+                  return Row(
+                    children: [
+                      Expanded(
+                          child:
+                              Divider(indent: 10, color: state == 0 ? Colors.grey[850] : Colors.white, thickness: 5)),
+                      Expanded(
+                          child:
+                              Divider(indent: 10, color: state == 1 ? Colors.grey[850] : Colors.white, thickness: 5)),
+                      Expanded(
+                          child:
+                              Divider(indent: 10, color: state == 2 ? Colors.grey[850] : Colors.white, thickness: 5)),
+                    ],
+                  );
+                },
+              ),
               _buildNextButton(),
             ],
           ),
