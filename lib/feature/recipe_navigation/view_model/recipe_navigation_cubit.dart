de1fd38d16202580e0a29eb33d1bd8982b2d477f
ufukyazici <ufukyazici.dev@gmail.com>
2024-01-04 19:01:08 +0300

    Add recipe navigation functionality

--- lib/feature/recipe_navigation/view_model/recipe_navigation_cubit.dart
+++ lib/feature/recipe_navigation/view_model/recipe_navigation_cubit.dart
@@ -8,7 +8,27 @@ part 'recipe_navigation_state.dart';
 class RecipeNavigationCubit extends Cubit<RecipeNavigationState> {
   RecipeNavigationCubit() : super(const RecipeNavigationState());
   late int remainingSeconds;
-  startRecipeNavigation() {}
+  List<dummyData> items = [
+    dummyData(duration: 10, step: "Step 1"),
+    dummyData(duration: 20, step: "Step 2"),
+    dummyData(duration: 30, step: "Step 3"),
+    dummyData(duration: 40, step: "Step 4"),
+    dummyData(duration: 50, step: "Step 5"),
+    dummyData(duration: 60, step: "Step 6"),
+    dummyData(duration: 70, step: "Step 7"),
+    dummyData(duration: 80, step: "Step 8"),
+    dummyData(duration: 90, step: "Step 9"),
+    dummyData(duration: 100, step: "Step 10"),
+  ];
+  startRecipeNavigation() {
+    for (var i = 0; i < items.length; i++) {
+      emit(state.copyWith(
+        currentStep: items[i].step,
+      ));
+      startTimer(items[i].duration);
+      Future.delayed(Duration(seconds: items[i].duration));
+    }
+  }
 
   startTimer(int seconds) {
     remainingSeconds = seconds;
@@ -26,9 +46,9 @@ class RecipeNavigationCubit extends Cubit<RecipeNavigationState> {
   }
 }
 
-// class dummyData {
-//   final int duration;
-//   final String step;
+class dummyData {
+  final int duration;
+  final String step;
 
-//   dummyData({required this.duration, required this.step});
-// }
+  dummyData({required this.duration, required this.step});
+}
