f63801b109f21b77c820dd792807830a772985b1
ufukyazici <ufukyazici.dev@gmail.com>
2024-04-27 19:21:51 +0300

    Update home_view.dart with recipe navigation feature

--- lib/feature/home/view/home_view.dart
+++ lib/feature/home/view/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:chefistant_food_recipe_helper/feature/home/model/recipe_model.dart';
 import 'package:chefistant_food_recipe_helper/feature/home/service/firebase_service.dart';
+import 'package:chefistant_food_recipe_helper/feature/recipe_navigation/view/recipe_navigation_view.dart';
 import 'package:chefistant_food_recipe_helper/product/widget/appbar/project_appbar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -32,15 +33,23 @@ class _HomeViewState extends State<HomeView> {
                   List<RecipeModel> recipe = recipes.map((document) => RecipeModel.fromJson(document.data())).toList();
                   return GridView.builder(
                     gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 200 / 250),
+                        const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, childAspectRatio: 140 / 150),
                     itemCount: recipe.length,
                     shrinkWrap: true,
                     itemBuilder: (context, index) {
                       DocumentSnapshot document = recipes[index];
+                      final documentID = document.id;
                       return Card(
                         child: ListTile(
-                          title: Text(document.id.toString()),
+                          title: Text(recipe[index].recipe?.name ?? ""),
                           subtitle: Text(recipe[index].recipe?.description ?? ""),
+                          onTap: () {
+                            Navigator.push(context, MaterialPageRoute(
+                              builder: (context) {
+                                return RecipeNavigationView(recipeNavigation: recipe[index].recipeNavigation ?? []);
+                              },
+                            ));
+                          },
                         ),
                       );
                     },
